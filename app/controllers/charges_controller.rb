fcd07cb91cfb846007c0dca1b3720a3b81dc6a31
Chris Kosednar <christopher.kosednar@gmail.com>
2016-01-31 16:37:57 -0800

    stripe subscription plan

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,18 +4,20 @@ class ChargesController < ApplicationController
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
+	    :card  => params[:stripeToken],
+	    :plan  => "90coffee30"
+
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => params[:amount],
-	    :description => 'Coffee Subscription',
-	    :currency    => 'usd'
-	  )
+	  # charge = Stripe::Charge.create(
+	  #  :customer    => customer.id,
+	  #  :amount      => params[:amount],
+	  # :description => 'Coffee Subscription',
+	  #  :currency    => 'usd'
+	  #)
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-    amount: params[:amount], description: charge.description, currency: charge.currency,
+    amount: params[:amount], description: 'Coffee Subscription', currency: 'usd',
     customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
 
 redirect_to purchase
